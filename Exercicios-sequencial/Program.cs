08fbfb92337a0ed94d525b4ae3a1380d572be1bb
Damiao <damiao.jose@hotmail.com>
2022-01-12 09:10:36 -0300

    primeiro commit csharp

---
+++ Exercicios-sequencial/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios_sequencial
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            Console.WriteLine("digite o valor do salario: ");
+            double salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vale transporte? (S = Sim / N + Não)");
+            char vale = char.Parse(Console.ReadLine());
+
+            if (vale == 'S' || vale == 's')
+                {
+                liquidoPassagem(salario);
+                }
+            else
+                {
+                liquidoSemPassagem(salario);
+                }
+
+            }
+
+        static void liquidoPassagem(double salario)
+            {
+            double descInss =  salario * 0.08;
+            Console.WriteLine("INSS " + descInss);
+            double descPassagem = salario * 0.06;
+            Console.WriteLine("Passagem " + descPassagem);
+            double valPassagem = 115.50;
+            double insalubridade = 220;
+
+            salario = salario - descInss - descPassagem + valPassagem + insalubridade;
+            Console.WriteLine(salario);
+            }
+        static void liquidoSemPassagem(double salario)
+            {
+            double descInss = salario - salario * 0.08;
+            double insalubridade = salario + 220;
+
+            salario = salario - descInss + insalubridade;
+            Console.WriteLine(salario);
+            }
+        }
+    }
